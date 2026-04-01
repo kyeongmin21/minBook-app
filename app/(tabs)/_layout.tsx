0066f710399c7d8ec8fbd7a654ffbf8184dcec70
kim kyeong min <kyeongmin21@naver.com>
2026-04-01 14:13:17 +0900

    로그인시 하단 Tab에서 프로필 나오도록 설정

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import {Image} from "expo-image";
-import {Text, StyleSheet} from 'react-native';
+import {Image} from 'expo-image';
+import {Text, StyleSheet, View} from 'react-native';
 import {Tabs} from 'expo-router';
 import {Colors} from '@/constants/theme';
+import {useAuthStore} from '@/store/authStore';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import {HapticTab} from '@/components/haptic-tab';
 import {useColorScheme} from '@/hooks/use-color-scheme';
 
-const TabLabel = ({focused, color, label}: { focused: boolean; color: string; label: string }) => (
+
+const TabLabel = ({focused, color, label}: {
+    focused: boolean;
+    color: string;
+    label: string
+}) => (
     <Text style={[styles.label, {color}, focused && styles.focusedLabel]}>
         {label}
     </Text>
@@ -14,6 +21,9 @@ const TabLabel = ({focused, color, label}: { focused: boolean; color: string; la
 
 export default function TabLayout() {
     const colorScheme = useColorScheme();
+    const {user, isLoggedIn, isInitialized} = useAuthStore();
+
+    if (!isInitialized) return null;
 
     return (
         <Tabs
@@ -23,44 +33,67 @@ export default function TabLayout() {
                 tabBarButton: HapticTab,
             }}>
             <Tabs.Screen
-                name="wishlist"
+                name='wishlist'
                 options={{
                     title: '북마크',
-                    tabBarLabel: (props) => <TabLabel {...props} label="북마크"/>,
+                    tabBarLabel: (props) => <TabLabel {...props} label='북마크'/>,
                     tabBarIcon: ({color}) => (
                         <Image
                             source={require('@/assets/images/heart.png')}
-                            style={{width: 30, height: 30}}
+                            style={{width: 28, height: 30}}
                         />
                     ),
                 }}
             />
             <Tabs.Screen
-                name="index"
+                name='index'
                 options={{
                     title: '홈',
-                    tabBarLabel: (props) => <TabLabel {...props} label="홈"/>,
+                    tabBarLabel: (props) => <TabLabel {...props} label='홈'/>,
                     tabBarIcon: ({color}) => (
                         <Image
                             source={require('@/assets/images/home.png')}
-                            style={{width: 35, height: 30}}
+                            style={{width: 32, height: 30}}
                         />
                     )
                 }}
             />
             <Tabs.Screen
-                name="read"
+                name='read'
                 options={{
                     title: '읽은 책',
-                    tabBarLabel: (props) => <TabLabel {...props} label=" 읽은책"/>,
+                    tabBarLabel: (props) => <TabLabel {...props} label=' 읽은책'/>,
                     tabBarIcon: ({color}) => (
                         <Image
                             source={require('@/assets/images/read.png')}
-                            style={{width: 30, height: 32}}
+                            style={{width: 28, height: 30}}
                         />
                     )
                 }}
             />
+
+
+            <Tabs.Screen
+                name='profile'
+                options={{
+                    title: '',
+                    tabBarLabel: (props) => <TabLabel {...props} label='프로필'/>,
+                    href: !isLoggedIn ? null : undefined, // 비로그인시 탭에서 숨김
+                    tabBarIcon: () => (
+                        user?.avatar_url ? (
+                            <Image
+                                source={{uri: user.avatar_url}}
+                                style={styles.profile}
+                            />
+                        ) : (
+                            <View style={styles.profile}>
+                                <Ionicons name='person-outline' size={20} color='#aaa'/>
+                            </View>
+                        )
+                    )
+                }}
+            />
+
         </Tabs>
     );
 }
@@ -73,4 +106,14 @@ const styles = StyleSheet.create({
     focusedLabel: {
         fontWeight: 'bold',
     },
+    profile: {
+        width: 30,
+        height: 30,
+        borderRadius: 50,
+        borderWidth: 1,
+        borderColor: '#E8E8E8',
+        marginRight: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
